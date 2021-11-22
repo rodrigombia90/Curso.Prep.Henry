1c4ee04e384da5efee71c383cd256133769f8f8a
rodrigombia90 <rodrigoalejandroloza11@gmail.com>
2021-11-22 20:42:08 -0300

    22/11

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,20 +28,20 @@ const nuevoModulo = 21 % 5 === 1.05;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  
+  return str;
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  
+  return x+y;
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  
+  return x-y;
 }
 
 function multiplica(x, y) {
